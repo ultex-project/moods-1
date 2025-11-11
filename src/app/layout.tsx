34179a8639240304829037f23cf3d1cf80c01550
Rida bahni <rida.bahni.00@gmail.com>
2025-11-11 21:45:21 +0100

    corrections

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={`${inter.className} bg-white text-[#2E4A35]`}>{children}</body>
         </html>
     );
 }
